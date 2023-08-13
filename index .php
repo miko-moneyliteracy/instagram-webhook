4dd378007ceb48cf60cdc63687ad4e497bfab62a
miko-moneyliteracy <142096957+miko-moneyliteracy@users.noreply.github.com>
2023-08-13 11:12:31 +0900

    index.php

---
+++ index .php
@@ -0,0 +1,11 @@
+<?php
+  file_put_contents('verify.log, print_r( $_GET, true ), FILE_APPEND);
+?>
+<html>
+    <head>
+        <title>Instagram Graph API Webhooks</title>
+    </head>
+    <body>
+        <h1>Instagram Graph API Webhooks</h1>
+    </body>
+</html>
